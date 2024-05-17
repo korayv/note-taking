611a30a85fe56b286dfe1b4a1d4eac8c11c9db38
korayv <koray.vatan@agu.edu.tr>
2024-05-17 17:18:06 +0300

    Notelisting is possible

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,27 +1,38 @@
 import NewNote from '~/components/newNote';
+import NoteList from '~/components/NoteList';
 import NewNotesStyles from '~/components/newNote.css';
 import { getStoredNotes, storeNotes } from '../data/notes';
 import { redirect } from '@remix-run/node';
-export default function NotesPage(){
-    return <main>
-        <NewNote />
+import { useLoaderData } from '@remix-run/react';
 
+export default function NotesPage() {
+    const notes = useLoaderData();
+    return (
+    <main>
+        <NewNote />
+        <NoteList notes={notes} />
     </main>
+);
 }
-export async function action({request}){
-  const formData = await request.formData();
-    const noteData = 
+export async function loader() {
+    const notes = await getStoredNotes();
+    return notes;
+}
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const noteData =
     {//Object.fromEntries(formData);
         title: formData.get('title'),
         content: formData.get('content')
     };
     const existingNotes = await getStoredNotes();
     noteData.id = new Date().toISOString();
-    const updatedNotes =existingNotes.concat(noteData);
+    const updatedNotes = existingNotes.concat(noteData);
     await storeNotes(updatedNotes);
     return redirect('/notes')
 }
 
-export function links(){
-     return [{ rel: 'stylesheet', href: NewNotesStyles}];
+export function links() {
+    return [{ rel: 'stylesheet', href: NewNotesStyles }];
 }
